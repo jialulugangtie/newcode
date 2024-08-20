e08c25d6d872969778653e390ff7aeb4a212d1c2
jialulugangtie <1812166290@qq.com>
2024-08-20 10:22:43 +0800

    complete item 1

--- newcode/newcode.cpp
+++ newcode/newcode.cpp
@@ -1,20 +1,37 @@
-﻿// newcode.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
+﻿#include <iostream>
+#include<vector>
+#include<unordered_map>
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int op_count{ 0 };
+    std::cin >> op_count;
+    std::unordered_map<int, int> warehouse;
+    int index{ -1 };
+    for (int i = 0; i < op_count; i++) {
+        std::cin >> index;
+        warehouse[index]++;
+    }
+
+    std::cin >> op_count;
+    for (int i = 0; i < op_count; i++) {
+        std::cin >> index;
+        warehouse[index]--;
+    }
+
+    std::vector<int> ans(16, 0);
+    for (const auto& [v1, v2] : warehouse) {
+        if (v2 >= 1 && v2 <= 15) {
+            ans[v2]++;
+        }
+    }
+
+    for (int i = 1; i <= 15; i++) {
+        std::cout << ans[i] << (i == 15 ? "\n" : " ");
+    }
+    return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
